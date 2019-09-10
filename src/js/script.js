62c70407760ff75ea00f5075fad09fc3d7123aed
MikhailKey <48489639+MikhailKey@users.noreply.github.com>
2019-09-10 13:47:23 +0400

    first page responsive

--- src/js/script.js
+++ src/js/script.js
@@ -2,91 +2,123 @@ $(document).ready(function(){
     $("#zaimy-carousel").owlCarousel({
         loop: true,
         items: 3,
-        autoplay : 5000,
+        video: true,
+        autoplay : 3000,
         autoplaySpeed: 1500,
         autoplayHoverPause: true,
         navElement: 'button',
         navText: ['<div class="svg" data-src="img/zaimy/arrow-left.svg"></div>', '<div class="svg" data-src="img/zaimy/arrow-right.svg"></div>'],
-	
         nav: true,
         margin:10,
     });
+
+    $("#saving-slider").owlCarousel({
+        loop: true,
+        items: 1,
+        animateOut: 'fadeOut',
+        autoHeight : true,
+        autoplay : 3000,
+        autoplaySpeed: 1500,
+        autoplayHoverPause: true,
+    });
+
     $('.svg').each(function() {
         var path = '../' + $(this).data('src');
         replace($(this), path);
     });
     
     function replace(elem, path) {
-        var s = new XMLSerializer();
+        let s = new XMLSerializer();
         $.ajax({
             url: path,
             success: function(data) {
-                var str = s.serializeToString(data);
+                let str = s.serializeToString(data);
                 elem.append(str);
                 elem.find('svg').unwrap();
               }
         });
     }
+    if ($('.navigation-bottom-list')[0])
+    {
+        function showTariff(...args)
+        {
+            let tariffButton = $(args[0]),
+            tariffWindow = $(args[1]),
+            closeButton = $(args[2]);
+        
+            tariffButton.click(function()
+            {
+                tariffWindow.fadeIn(500);
+            })
+
+            closeButton.click(function()
+            {
+                tariffWindow.fadeOut(500);
+            })
+        }
+
+        function hideTariff(arg)
+        {
+            $(document).mouseup(function (e){ 
+                let div = $(arg); 
+                if (!div.is(e.target)
+                    && div.has(e.target).length === 0) { 
+                    div.fadeOut(); 
+                }
+            });
+        }
+
+        function showModal(...args)
+        {
+            let navButton = $(args[0]),
+            navWindow = $(args[1]);
+
+            navButton.click(function()
+            {
+                navWindow.fadeToggle(300);
+            })   
+        } 
+
+        function hideModal(...args)
+        {
+            $(document).mouseup(function (e){ 
+                let div = $(args[0]); 
+                if (!div.is(e.target)
+                    && div.has(e.target).length === 0) { 
+                    div.fadeOut(); 
+                }
+            });
+        }
+       
+        showModal('.navigation-bottom-menu__hamburger', '.navigation-bottom-list');
+        hideModal('.navigation-bottom-list');
+        showTariff('.saving-tariff-button button', '.tariff-info', '.tariff-close');
+        hideTariff('.tariff-info');
+    }
+    else 
+    {
+        function showBusinessModal(...args)
+        {
+            let navButton = $(args[0]),
+            navWindow = $(args[1]);
+
+            navButton.click(function()
+            {
+                navWindow.fadeToggle(300);
+            })   
+        } 
+
+        function hideBusinessModal(...args)
+        {
+            $(document).mouseup(function (e){ 
+                let div = $(args[0]); 
+                if (!div.is(e.target)
+                    && div.has(e.target).length === 0) { 
+                    div.fadeOut(); 
+                }
+            });
+        }
+        showBusinessModal('.b-navigation-bottom-menu__hamburger', '.b-navigation-bottom-list');
+        hideBusinessModal('.b-navigation-bottom-list');
+    }
 });
-
-
-let pageName = window.location.pathname;
-
-if (pageName.includes('business')) 
-{
-    function showBusinessNav(...args) 
-    {
-        let navButton = document.querySelector(args[0]),
-            navMenu = document.querySelector(args[1]);
-        navButton.addEventListener('click', function()
-        {
-            navMenu.classList.toggle('fade');
-        })
-    }
-
-    function hideBusinessNav(...args) 
-    {
-        let navButton = document.querySelector(args[0]),
-            navMenu = document.querySelector(args[1]);
-        document.addEventListener('mouseup', function(e) 
-        {
-            if (!navMenu.contains(e.target) && !navButton.contains(e.target)) 
-            {
-                navMenu.classList.remove('fade');
-            }
-        })
-    }
-
-    showBusinessNav('.b-navigation-bottom-menu__hamburger', '.b-navigation-bottom-list');
-    hideBusinessNav('.b-navigation-bottom-menu__hamburger', '.b-navigation-bottom-list');
-} 
-else 
-{
-    function showNav(...args) 
-    {
-        let navButton = document.querySelector(args[0]),
-            navMenu = document.querySelector(args[1]);
-
-        navButton.addEventListener('click', function()
-        {
-            navMenu.classList.toggle('fade');        
-        })
-    }
-
-    function hideNav(...args) 
-    {
-        let navButton = document.querySelector(args[0]),
-            navMenu = document.querySelector(args[1]);
-
-        document.addEventListener('mouseup', function(e) 
-        {
-            if (!navMenu.contains(e.target) && !navButton.contains(e.target)) 
-            {
-                navMenu.classList.remove('fade');
-            }
-        })
-    }
-
-    showNav('.navigation-bottom-menu__hamburger', '.navigation-bottom-list');
-    hideNav('.navigation-bottom-menu__hamburger', '.navigation-bottom-list');
-}
