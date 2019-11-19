11401252929423eb06a6b6ec6e06130dc1c95abe
faceru <facedirect@yandex.ru>
2019-11-19 23:48:31 +0400

    Review

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+
+// Ты пишешь весь js в одном файле, почему не комментируешь, что к какой странице относиться
+// можно для этого создать большие функции с именами страниц и вызывать внутри функции которые относятся к этой странице, а еще потом для оптимизации написать условия, которые не будут отрабатывать скрипты там где не надо
+
 $(document).ready(function () {
   $(".zaimy-carousel").owlCarousel({
     loop: true,
@@ -8,7 +12,7 @@ $(document).ready(function () {
     autoplaySpeed: 1500,
     autoplayHoverPause: true,
     navElement: 'button',
-    navText: ['<div class="svg" data-src="img/zaimy/arrow-left.svg"></div>', '<div class="svg" data-src="img/zaimy/arrow-right.svg"></div>'],
+    navText: ['<div class="svg" data-src="img/zaimy/arrow-left.svg"></div>', '<div class="svg" data-src="img/zaimy/arrow-right.svg"></div>'], // вынести в глобальную константу этот массив
     nav: true,
     margin: 10,
     responsive: {
@@ -25,7 +29,18 @@ $(document).ready(function () {
       }
     }
   });
-
+  // зачем спамить вызов слайдеров, если у тебя половина параметров повторяется
+  const arrowsForSlider = ['<div class="svg" data-src="img/zaimy/arrow-left.svg"></div>', '<div class="svg" data-src="img/zaimy/arrow-right.svg"></div>'];
+  //пример реализации
+  const slider = (selector, params) => {
+    $(selector).owlCarousel({
+      loop: true,
+      mouseDrag: false,
+      navNext: arrowsForSlider,
+      ...params /// уникальные параметры
+    });
+  }
+  //пример реализации
   $("#saving-slider").owlCarousel({
     loop: true,
     items: 1,
@@ -64,19 +79,19 @@ $(document).ready(function () {
   function easySVG() {
     const svgArr = document.querySelectorAll('.svg');
 
-    for (var i = 0; i < svgArr.length; i++) {
+    for (var i = 0; i < svgArr.length; i++) { // у тебя @babel в gulp замешан, зачем пишешь обычные циклы, когда можно forEach, обычные плохо читаются, плюс не оптимизированы, а вообще лучше использовать lodash
       const path = svgArr[i].dataset.src;
-      replaceSvg(svgArr[i], path);
+      replaceSvg(svgArr[i], path); // что такое replaceSvg WTF, давай нормальные имена или комментируй
     }
   }
 
-  function replaceSvg(elem, path) {
+  function replaceSvg(elem, path) { // что блять за монсор, ты как бы троль полюбому можно проще, у тебя подключен jquery, ты на нем пишешь, но вместо ajax, юзаешь чистый xmh
     if ('ActiveXObject' in window) {
-      let IErequest = new ActiveXObject('MSXML2.XMLHTTP');
+      let IErequest = new ActiveXObject('MSXML2.XMLHTTP');//это не читаемо, комментируй или переписывай
       IErequest.onReadyStateChange = function () {
         if (IErequest.readyState === 4 && IErequest.status === 200) {
-          const attr = elem.getAttribute('class');
-          const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
+          const attr = elem.getAttribute('class');// а elem.classList тебя не устраивает?
+          const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3); //wtf что за нановычисления, можно проще, еще раз wtf вообще не смог понять зачем, да и не хочу
 
           if (elem.parentNode) {
             elem.insertAdjacentHTML('afterend', IErequest.responseText);
@@ -96,7 +111,7 @@ $(document).ready(function () {
       let request = new XMLHttpRequest();
       request.open('GET', path);
       request.addEventListener("readystatechange", () => {
-        if (request.readyState === 4 && request.status === 200) {
+        if (request.readyState === 4 && request.status === 200) {// повторяющийся код, нельзя один обработчик на статус сделать в функции и в нее прокидывать уникальный код??
           const attr = elem.getAttribute('class');
           const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
 
@@ -116,13 +131,25 @@ $(document).ready(function () {
     }
   }
 
-  function SVGrefresh() {
+  function SVGrefresh() {// WTF????????????????????????????????????????????????????????????????????????????????????! функция которая, вызывают функцию, которая нигде во всем проекте не вызывается
+    // это блять как function error() {console.log('error')}
     easySVG();
   }
-
+  // ты вызываешь и описываешь функцию там где хочешь и когда хочешь, опиши все функцию за пределами document Ready, и вызывай их в Ready, а так какой смысл в функции если ты ее описываешь и сразу же вызываешь а дальше описываешь другую функцию, это не читаем
+  // И так еще получаеться, что функции у тебя создаются только когда страница уже загрузилась, а должны создаваться во время загрузки, а выполняться уже когда загрузилась
   easySVG();
 
-  function mapProcess() {
+  function mapProcess() { // какой смысл в функции, если в ней все константно, и ее нельзя переиспользовать
+    // пример реализации 
+    const mapProcess = (selectors) => {
+      doSomethingWithSelectors();
+    }
+    mapProcess({
+      mapButton:  $('.connect-button'),
+      townTitle: $('.town-modal-title'),
+    })
+    // пример реализации 
+
     let mapButton = $('.connect-button'),
       closeButton = $('.connect-modal-scheme button'),
       townTitle = $('.town-modal-title'),
@@ -161,7 +188,7 @@ $(document).ready(function () {
     }
     handleChangeAdress()
 
-    function sendAttributes(item) {
+    function sendAttributes(item) {// Зачем эта палатненка, а нельзя в атрибут пихать все разметку сразу в 1 атрибут, а не писать вот этот ад
       $(townTitle).html(item.attr('data-town'));
       $(townStatus).html(item.attr('data-status'));
       $(townAdress).html(item.attr('data-adress'));
@@ -176,10 +203,11 @@ $(document).ready(function () {
 
     }
 
-    mapButton.each(function (i, button) {
+    mapButton.each(function (i, button) { // то есть там выше ты пишешь for, а тут each из jquery ну ты троль конеш, такое впечатление, что js 3 разных человека пишет, и 2 из них по пьяне
       $(button).on('click', function () {
         $('.connect-modal').fadeIn(250);
-        $('body').css('overflow', 'hidden');
+        $('body').css('overflow', 'hidden');// не очень оптимизированный метод, лучше делать через .classList += 'activeClass' или .addClass из jquery, хотя с учетом того что ты тут жмешь джиквери, то как бы о чем базар
+
 
         sendAttributes($(this));
         let coordinates = JSON.parse($(this).attr('data-coordinates'));
@@ -188,13 +216,13 @@ $(document).ready(function () {
       })
     })
 
-    $(closeButton).on('click', function () {
-      $('.connect-modal').fadeOut(250);
-      $('body').css('overflow', 'visible');
+    $(closeButton).on('click', function () { // вот тут я уже не вьехал откуда closeButton
+      $('.connect-modal').fadeOut(250); // тоже лучше делать с помощью addClass анимацию
+      $('body').css('overflow', 'visible'); // уже писал
     })
 
     function hideMap() {
-      $(document).mouseup(function (e) {
+      $(document).mouseup(function (e) {  // я не знаю что это, но document mouseUp выглядит супер странно, если хотели закрывать что-то по клику вне элементы, есть куча способов проще
         let div = $('.connect-modal-container');
         if (!div.is(e.target)
           && div.has(e.target).length === 0) {
@@ -203,7 +231,7 @@ $(document).ready(function () {
         }
       });
     }
-    hideMap()
+    hideMap() // опять хуй пойми чо за вызов, и что он тут делает
 
 
   }
@@ -213,7 +241,15 @@ $(document).ready(function () {
     if ($("#map").length > 0) {
       ymaps.ready(function () {
         let description = ''
-        if (JSON.stringify(coordinates) === '[51.531825572387724,46.01106200000002]') {
+        if (JSON.stringify(coordinates) === '[51.531825572387724,46.01106200000002]') {// а про switch case не знаешь? JSON.stringify(coordinates) не проще в переменную вынести? 
+          // координаты в объект , не читаем, что за координаты откуда взялись, а если бы не было, description, я бы вообще прочитать не смог наверное
+              // пример реализации 
+
+          const adresses = {
+            sover: '[51.531825572387724,46.01106200000002]'
+          }
+              // пример реализации 
+
           description = 'Адрес: Ул. Советская 86/70'
         } else if (JSON.stringify(coordinates) === '[51.498775094836816,46.11590139814754]') {
           description = 'ул. М. Горького, 28'
@@ -250,17 +286,17 @@ $(document).ready(function () {
     function changeAdress(pressedButton) {
       let items = $('.connect-info-item');
       items.each(function (i, item) {
-        if ($(item).hasClass('connect-info-item__order')) {
+        if ($(item).hasClass('connect-info-item__order')) { // connect-info-item__order три раза повторяешь константу, выноси в переменные
           $(item).removeClass('connect-info-item__order');
         }
-        if ($(item).find('h1').text().indexOf(pressedButton) > -1) {
+        if ($(item).find('h1').text().indexOf(pressedButton) > -1) {// на всякий случай indexOf загрузит тебе страницу секунд на 15 страницу , если данных будет много, самый медленный метод поиска
           $(item).addClass('connect-info-item__order');
         }
       })
     }
 
     function orderContacts() {
-      if ($('.connect-info-item').length) {
+      if ($('.connect-info-item').length) { // блять нахуй выше переменная с этим селектором, вынеси ее в глобальную область и используй сразу в 2х функциях
         let townData = JSON.parse(localStorage.getItem('townData'));
         changeAdress(townData.town);
       }
@@ -281,7 +317,7 @@ $(document).ready(function () {
     navButton.on('click', function () {
       closeModal()
       if ($('.connect-info-item').length) {
-        changeAdress($(this).text())
+        changeAdress($(this).text()) // в переменные такие вещи блять
       }
     })
 
@@ -298,7 +334,7 @@ $(document).ready(function () {
 
   showConnect()
 
-  function hideConnect() {
+  function hideConnect() {// в чем смысл функции если ты выше пишешь такую же функцию только с другим селектором, wtf, Функции нужны, чтобы их переиспользовать, а не дублиовать код
     $(document).mouseup(function (e) {
       let div = $('.navtop-modal');
       if (!div.is(e.target)
@@ -310,7 +346,7 @@ $(document).ready(function () {
 
   hideConnect()
 
-  function sendAdress() {
+  function sendAdress() {// опять же в чем смысл если ее нельзя переиспользовать, и все константно
     let firstPhone = document.querySelectorAll('.phone-first'),
       secondPhone = document.querySelectorAll('.phone-second'),
       adress = document.querySelectorAll('.adress-title'),
@@ -336,7 +372,7 @@ $(document).ready(function () {
       })
     }
     function getStorage() {
-      let townData = JSON.parse(localStorage.getItem('townData'));
+      let townData = JSON.parse(localStorage.getItem('townData')); // зачем в кеш? Ладно не суть, суть в том, что get из кеша, это синхронная операция, ожидай ошибки, или пиши Promise
       town.forEach(function (town) { town.innerHTML = townData.town });
       adress.forEach(function (adress) { adress.innerHTML = townData.adress });
       firstPhone.forEach(function (phone) { phone.innerHTML = townData.firstPhone });
@@ -351,7 +387,7 @@ $(document).ready(function () {
 
   sendAdress()
 
-  if ($('.navigation-bottom-list')[0]) {
+  if ($('.navigation-bottom-list')[0]) { // а почему нельзя было написать $('.navigation-bottom-list')[0] && $('.calc-item').length и вообще что ты проверяешь $('.calc-item').length, не являеться ли длинна undefined?
     if ($('.calc-item').length) {
 
       let stavkaRef = 12;
@@ -360,7 +396,7 @@ $(document).ready(function () {
       function checkProgram() {
         if (checkButton.checked) {
           comfort()
-          $('.saving-options__button').eq(1).addClass('saving-options__button__active');
+          $('.saving-options__button').eq(1).addClass('saving-options__button__active');// да вы ебанутые? в чем опять смысл функции, если тут это написано
           $('.saving-options__program').eq(1).addClass('saving-options__program__active');
           $('.saving-options__button').eq(0).removeClass('saving-options__button__active');
           $('.saving-options__program').eq(0).removeClass('saving-options__program__active');
@@ -375,15 +411,14 @@ $(document).ready(function () {
       checkButton.addEventListener('click', function () {
         checkProgram()
       })
-      //с возможностью снятия
       function comfort() {
         let comfortFullSumm = 0,
           comfortSummSber = $('.summ-value').val(),
           comfortMonth = $('.time-value').val(),
           percentArr;
 
-        if (comfortSummSber >= 3000 && comfortSummSber <= 199999) {
-          percentArr = [{
+        if (comfortSummSber >= 3000 && comfortSummSber <= 199999) {// что это за цифры, откуда ты их взял? я не понимаю, можно комментировать или довать имена через пенеменные
+          percentArr = [{ // рили? что обозначают свойства эти
             monthArr: 3,
             stavkaArr: 3
           },
@@ -464,12 +499,11 @@ $(document).ready(function () {
           } else {
             nalog = 0;
           }
-          $('.calc-result__info')[0].innerHTML = stavkaArr + '%';
+          $('.calc-result__info')[0].innerHTML = stavkaArr + '%'; 
           $('.calc-result__info')[1].innerHTML = Math.round(nalog) + 'р.';
           $('.calc-result__info')[2].innerHTML = Math.round(comfortFullSumm) + 'р.';
         }
       }
-      //без возможности снятия 
       function dohod() {
         let dohodFullSumm = 0,
           dohodSummSber = $('.summ-value').val(),
